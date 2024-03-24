9a9243f6485f9c61eff0ff8ebb6af8d553b9e09d
Indra Sah Noeldy <146718520+indra87g@users.noreply.github.com>
2024-03-24 22:11:24 +0700

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide' );
+    splide.mount();
+  } );
+
+new Splide( '.splide' ).mount();
+console.log("Welcome to my website!");
